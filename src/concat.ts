dbe1cf5d34f101f8430d0f333eed04aef717665f
Moamen Hredeen <moamen@hredeen.com>
2025-05-30 13:03:09 +0200

    challenge(concat): array, easy (could not solve it by myself)

---
+++ src/concat.ts
@@ -0,0 +1,36 @@
+/**
+ * # Concat
+ * by Andrey Krasovsky (@bre30kra69cs) #easy #array
+ *
+ * ## Question
+ *
+ * Implement the JavaScript `Array.concat` function in the type system. A type takes the two arguments. The output should be a new array that includes inputs in ltr order
+ *
+ * For example:
+ *
+ * ```ts
+ * type Result = Concat<[1], [2]> // expected to be [1, 2]
+ * ```
+ *
+ * > View on GitHub: https://tsch.js.org/533
+ */
+type Concat<T extends readonly unknown[], U extends readonly unknown[]> =   [...T, ...U]
+
+// -------------------------- Test Cases --------------------------
+
+import type { Equal, Expect } from './utils'
+
+
+const tuple = [1] as const
+type test = Concat<[1, 2], [3]>
+
+type cases = [
+    Expect<Equal<Concat<[], []>, []>>,
+    Expect<Equal<Concat<[], [1]>, [1]>>,
+    Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
+    Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+    Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+]
+
+// @ts-expect-error
+type error = Concat<null, undefined>
