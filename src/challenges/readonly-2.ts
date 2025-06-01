cd8e0ef8f6bcb90ac81d99a2557c6dd7a68ad4fa
Moamen Hredeen <moamen@hredeen.com>
2025-06-01 18:21:44 +0200

    challenge(readoly2): default key type as object keys

--- src/challenges/readonly-2.ts
+++ src/challenges/readonly-2.ts
@@ -33,7 +33,7 @@
  */
 
 
-type MyReadonly2<T, K extends keyof T = any> = {
+type MyReadonly2<T, K extends keyof T = keyof T> = {
 	+readonly [P in keyof T as P extends K ? P : never]: T[P]
 } & {
 	[P in keyof T as P extends K ? never : P]: T[P]
